621fb330abac072f4da758c4fe9722a10f3ce6b3
Łukasz Jasionowski <jasionowski.lukasz@gmail.com>
2022-02-09 00:53:45 +0100

    Adding a method to fire an employee

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Boss {
     Integer cash = 2000;
     public Integer employees = 0;
@@ -16,12 +19,15 @@ public class Boss {
     Projects hard2 = new Projects("Przepisy Joli", "Crazy Jola", 5, 1000, 4000, 2, "Trudny", 2, 1, 0, 0, 1, 0);
     Projects hard3 = new Projects("Apka dla statystyk w grze", "RPG Nation", 10, 1000, 5000, 2, "Trudny", 4, 2, 0, 0, 2, 0);
     Projects hard4 = new Projects("Blog", "Michał Chlebek", 4, 500, 2000, 1, "Trudny", 1, 1, 0, 0, 2, 0);
+    public ArrayList<String> myEmployeesName = new ArrayList<String>();
     public Employee myEmployee;
-    Employee employee1 = new Employee("Jacek", 2000);
+    Employee employee1 = new Employee("Jacek", 1000);
     Employee employee2 = new Employee("Młodziak", 1000);
     Employee employee3 = new Employee("Aśka", 3000);
     Employee employee4 = new Employee("Fredryk", 2000);
     Employee employee5 = new Employee("Michał", 4000);
+    Scanner scan = new Scanner(System.in);
+    String name;
 
     public Projects getMyProject() {
         return myProject;
@@ -71,7 +77,8 @@ public class Boss {
     void showWhoWantToWork() {
         System.out.println("Lista dostępnych pracowników: \n" + employee1 + "\n" + employee2 + "\n" + employee3 + "\n" + employee4 + "\n" + employee5);
     }
-    void availableEmployee(){
+
+    void availableEmployee() {
         System.out.println("Osoby, które chcą u Ciebie pracować: \n" + employee1 + "\n" + employee2 + "\n" + employee3 + "\n" + employee4 + "\n" + employee5 + "\n" + "Aktualny stan konta firmowego: " + this.cash + "PLN");
     }
 
@@ -80,6 +87,7 @@ public class Boss {
             case "Jacek":
                 if (this.cash >= employee1.salary) {
                     this.myEmployee = employee1;
+                    myEmployeesName.add("Jacek");
                     employees++;
                     System.out.println("Zatrudniłeś: " + this.getMyEmployee());
                     this.cash -= employee1.salary;
@@ -91,6 +99,7 @@ public class Boss {
             case "Młodziak":
                 if (this.cash >= employee2.salary) {
                     this.myEmployee = employee2;
+                    myEmployeesName.add("Młodziak");
                     employees++;
                     System.out.println("Zatrudniłeś: " + this.getMyEmployee());
                     this.cash -= employee2.salary;
@@ -102,6 +111,7 @@ public class Boss {
             case "Aśka":
                 if (this.cash >= employee3.salary) {
                     this.myEmployee = employee3;
+                    myEmployeesName.add("Aśka");
                     employees++;
                     System.out.println("Zatrudniłeś: " + this.getMyEmployee());
                     this.cash -= employee3.salary;
@@ -113,6 +123,7 @@ public class Boss {
             case "Fredryk":
                 if (this.cash >= employee4.salary) {
                     this.myEmployee = employee4;
+                    myEmployeesName.add("Fredryk");
                     employees++;
                     System.out.println("Zatrudniłeś: " + this.getMyEmployee());
                     this.cash -= employee4.salary;
@@ -124,6 +135,7 @@ public class Boss {
             case "Michał":
                 if (this.cash >= employee5.salary) {
                     this.myEmployee = employee5;
+                    myEmployeesName.add("Michał");
                     employees++;
                     System.out.println("Zatrudniłeś: " + this.getMyEmployee());
                     this.cash -= employee5.salary;
@@ -137,8 +149,27 @@ public class Boss {
                 break;
         }
     }
-    public String getMyEmployee(){
+
+    public String getMyEmployee() {
         return myEmployee.name;
     }
 
+    public void fireTheEmployee() {
+        if (myEmployeesName.isEmpty()) {
+            System.out.println("Nie posiadasz pracowników!");
+
+        } else {
+            System.out.println("Lista pracowników: " + myEmployeesName);
+            System.out.println("Wpisz imię osoby, którą chcesz zwolnić: ");
+            name = scan.next();
+            if (!myEmployeesName.contains(name)) {
+                System.out.println("Błędne imię, sprawdź pisownię!");
+            } else {
+                myEmployeesName.remove(name);
+                System.out.println("Pracownik " + name + " został zwolniony");
+                cash -= 500;
+                System.out.println("Musiałeś ponieść koszty zwolnienia pracownika: - 500PLN\nAktualny stan konta: " + cash + "PLN");
+            }
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Integer currentDay = 0;
+        Integer currentDay = 1;
         int easyMediumSelect = randomNumberGenerator(1, 8); //Wybór bez Trudnych projektów
         int fullSelect = randomNumberGenerator(1, 12); //Pełen wybór
         Integer selection = 0;
@@ -18,53 +18,58 @@ public class Main {
         Boss employees = new Boss();
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("WITAJ W SYMULACJI FIRMY IT!\nRozpocznij rozgrywkę wybierając jedną z opcji:\n" +
-                "1.Podpisz nową umowę\n2.Zatrudnij pracownika\n3.Pracuj(programowanie)\n4.Testuj kod\n" +
-                "5.Oddaj gotowy projekt\n6.Zatrudnij pracownika\n7.Zwolnij pracownika\n8.Rozliczenia z urzędami");
-        selection = scan.nextInt();
+        System.out.println("WITAJ W SYMULACJI FIRMY IT!\nRozpocznij rozgrywkę wybierając jedną z opcji:");
+        do {
+            System.out.println("1.Podpisz nową umowę\n2.Zatrudnij pracownika\n3.Pracuj(programowanie)\n4.Testuj kod\n" +
+                    "5.Oddaj gotowy projekt\n6.Zatrudnij pracownika\n7.Zwolnij pracownika\n8.Rozliczenia z urzędami" + "\nDzień " + currentDay);
+            selection = scan.nextInt();
 
-        switch (selection) {
-            case 1: //Podpisanie nowej umowy
-                if (boss.employees == 0 && boss.myProject == null) {
-                    boss.setMyProject(easyMediumSelect);
-                    System.out.println("PODPISANO UMOWĘ:\n" + boss.getMyProject());
-                } else if (boss.employees >= 1) {
-                    employees.setMyProject(fullSelect);
-                    System.out.println("PODPISANO UMOWĘ:\n" + employees.getMyProject());
-                } else {
-                    System.out.println("Brak możliwości podpisania nowej umowy!");
-                }
-                currentDay++;
-                break;
-            case 2: //Dzień na szukanie pracownika
-                boss.showWhoWantToWork();
-                currentDay++;
-                break;
-            case 3:
+            switch (selection) {
+                case 1: //Podpisanie nowej umowy
+                    if (boss.employees == 0 && boss.myProject == null) {
+                        boss.setMyProject(easyMediumSelect);
+                        System.out.println("PODPISANO UMOWĘ:\n" + boss.getMyProject());
+                        currentDay++;
+                    } else if (boss.employees >= 1) {
+                        employees.setMyProject(fullSelect);
+                        System.out.println("PODPISANO UMOWĘ:\n" + employees.getMyProject());
+                        currentDay++;
+                    } else {
+                        System.out.println("Brak możliwości podpisania nowej umowy!");
+                    }
+                    break;
+                case 2: //Dzień na szukanie pracownika
+                    boss.showWhoWantToWork();
+                    currentDay++;
+                    break;
+                case 3:
 //Dzień na programowanie
-                break;
-            case 4:
+                    break;
+                case 4:
 //Dzień na testowanie
-                break;
-            case 5:
+                    break;
+                case 5:
 //Oddanie gotowego projektu
-                break;
-            case 6: //Zatrudnienie pracownika
-                boss.availableEmployee();
-                System.out.println("Wpisz imię pracownika, którego chcesz zatrudnić: ");
-                Scanner scanner1 = new Scanner(System.in);
-                String name = scan.next();
-                boss.setMyEmpolyee(name);
-                currentDay++;
-                break;
-            case 7:
-//Zwolnienie pracownika
-                break;
-            case 8:
+                    break;
+                case 6: //Zatrudnienie pracownika
+                    boss.availableEmployee();
+                    System.out.println("Wpisz imię pracownika, którego chcesz zatrudnić: ");
+                    Scanner scanner1 = new Scanner(System.in);
+                    String name = scan.next();
+                    boss.setMyEmpolyee(name);
+                    currentDay++;
+                    break;
+                case 7: //Zwolnienie pracownika
+                    boss.fireTheEmployee();
+                    currentDay++;
+                    break;
+                case 8:
 //Dzień na rozliczenie z urzędami
-                break;
-            default:
-                System.out.println("BŁĄD! Wybierz liczbę z zakresu 1-8");
-        }
+                    break;
+                default:
+                    System.out.println("BŁĄD! Wybierz liczbę z zakresu 1-8");
+            }
+        } while (boss.cash >= 0);
+        System.out.println("Przegrałeś");
     }
 }
